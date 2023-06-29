7210b8efdd5e5781447836087d1ecd5521c22ee3
germanoMartinss <germanopmartins91@gmail.com>
2023-06-28 22:02:54 -0300

    Correcao JS

--- script.js
+++ script.js
@@ -5,20 +5,26 @@ function string2Number(valor) {
 
 function digito(dig) {
   if (res.value.length < 12) {
-    if (res.value != "0") 
-    res.value = res.value + dig;
+    if (res.value != "0") res.value = res.value + dig;
     else res.value = dig;
   }
 }
 
 function total() {
-    if (op.value === "+")
-    res.value = parseFloat(vl.value) + parseFloat(res.value);
-    else if (op.value === "-")
-    res.value = parseFloat(vl.value) - parseFloat(res.value);
-    else if (op.value === "X")
-    res.value = parseFloat(vl.value) * parseFloat(res.value);
-    else if (op.value === "/")
+  if (op.value === "+")
+    res.value = parseFloat(v1.value) + parseFloat(res.value);
+  else if (op.value === "-")
+    res.value = parseFloat(v1.value) - parseFloat(res.value);
+  else if (op.value === "X")
+    res.value = parseFloat(v1.value) * parseFloat(res.value);
+  else if (op.value === "/") {
     if (res.value !== 0)
-    res.value = parseFloat(vl.value) / parseFloat(res.value)
+      res.value = parseFloat(v1.value) / parseFloat(res.value);
+  }
+}
+
+function operacao(ope) {
+    v1.value = res.value;
+    op.value = ope;
+    res.value = "0";
 }
