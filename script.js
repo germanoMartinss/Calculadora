cffa4a067808de05b5c7c87a5b5beaba98e134cb
germanoMartinss <germanopmartins91@gmail.com>
2023-06-28 21:59:34 -0300

    String Digito E Total Function

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 function string2Number(valor) {
-    valor = valor.replace(",", ".");
-    return(parseFloat(valor));
+  valor = valor.replace(",", ".");
+  return parseFloat(valor);
+}
+
+function digito(dig) {
+  if (res.value.length < 12) {
+    if (res.value != "0") 
+    res.value = res.value + dig;
+    else res.value = dig;
+  }
+}
+
+function total() {
+    if (op.value === "+")
+    res.value = parseFloat(vl.value) + parseFloat(res.value);
+    else if (op.value === "-")
+    res.value = parseFloat(vl.value) - parseFloat(res.value);
+    else if (op.value === "X")
+    res.value = parseFloat(vl.value) * parseFloat(res.value);
+    else if (op.value === "/")
+    if (res.value !== 0)
+    res.value = parseFloat(vl.value) / parseFloat(res.value)
 }
